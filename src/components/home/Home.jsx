8496dcfcbae3a4418472504d077d3e1d6bdeee0b
Jot4ril <jamesthomasogar@gmail.com>
2022-09-12 16:25:09 +0100

    Header of the Application

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './home.scss';
+import Navbar from '../navbar/Navbar';
+
+export default function Home() {
+  return (
+    <div className="home">
+      <Navbar />
+    </div>
+  );
+}
