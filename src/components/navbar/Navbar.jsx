5c40f78d945bab310c962e4e714771c1d5fe68c0
Jot4ril <jamesthomasogar@gmail.com>
2022-09-17 18:58:02 +0100

    Addition of Scrolling effect to the header

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,19 @@
+import React, { useState } from 'react';
 import './navbar.scss';
 
-import React from 'react';
 import { Search, Bell } from 'react-bootstrap-icons';
 import image from './image.jpg';
 
 export default function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+
   return (
-    <div className="navbar">
+    <div className={isScrolled ? 'navbar scrolled' : 'navbar'}>
       <div className="container">
         <div className="left">
           <svg
